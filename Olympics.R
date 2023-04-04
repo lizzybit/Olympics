0b5a096454afe6d8aaaffe5e9f8bb4d20320a304
Elizabeth <elizabethstorey@live.com>
2023-04-04 17:19:02 +0100

    Updated R

--- Olympics.R
+++ Olympics.R
@@ -8,6 +8,7 @@ install.packages("repr")
 install.packages("tidyverse")
 install.packages("readxl")
 install.packages("knitr")
+install.packages("rmarkdown")
 library(readr)
 library(sqldf)
 library(calibrate)
@@ -15,6 +16,7 @@ library(repr)
 library(tidyverse)
 library(readxl)
 library(knitr)
+library(rmarkdown)
 
 ###########################################################
 ###########################################################
@@ -23,7 +25,9 @@ setwd("/Users/elizabeth/Documents/GitHub/Olympics")
 getwd()
 
 olympics <- read.csv("athlete_events.csv", header = TRUE)
+
 head(olympics)
+dim(olympics)
 str(olympics)
 
 ###########################################################
@@ -36,7 +40,25 @@ null_values <- colSums(is.na(olympics))
 
 kable(null_values, format =  'markdown')
 
-# 2. Fill the missing values in the column Medal with string of 'NA'
-olympics$Medal[is.na(olympics$Medal)] <- "NA"
+# 2. Fill the missing values in the column Medal with string of 'DNW'
+olympics$Medal[is.na(olympics$Medal)] <- "DNW"
 
 sum(is.na(olympics$Medal))
+
+# 3. Replace missing values in height, weight and age with mean
+olympics$Height[is.na(olympics$Height)] <- mean(olympics$Height, na.rm = TRUE)
+olympics$Weight[is.na(olympics$Weight)] <- mean(olympics$Weight, na.rm = TRUE)
+olympics$Age[is.na(olympics$Age)] <- mean(olympics$Age, na.rm = TRUE)
+
+null_values <- colSums(is.na(olympics))
+kable(null_values, format =  'markdown')
+
+# 4. Drop unused games column as it contains data already found in year and season column
+
+olympics$Games <- NULL
+
+###########################################################
+###########################################################
+## Exploratory Data Analysis
+
+
